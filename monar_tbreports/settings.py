8477cf8eff7e6d0e1b4c30fe34b07dfd7896cc44
caiovictormc <caiovictormc@gmail.com>
2019-11-05 00:17:57 -0300

    Update static settings

--- monar_tbreports/settings.py
+++ monar_tbreports/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'monar_tbreports.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,5 +123,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_DIRS = [
+    "static",
+]
 
-STATIC_URL = '/static/'
+
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
